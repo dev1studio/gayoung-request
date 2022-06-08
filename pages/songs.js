25945f58031993e25a2c8dd39df4912990af9b85
Chloe <gtalk.with.nani@gmail.com>
2022-06-08 12:00:43 +0900

    Create a search function
    - 검색 기능을 작성하다

---
+++ pages/songs.js
@@ -0,0 +1,32 @@
+const songs = [
+  {
+    id: 1,
+    name: "스파이걸스 간첩소녀 spygirls 아빠컵엄마컵애기컵송",
+    singer: "간첩소녀 (Spygirls)",
+    album: "아빠컵 엄마컵 애기컵 송 (2018)",
+    title: "아빠컵 엄마컵 애기컵 송",
+  },
+  {
+    id: 2,
+    name: "스파이걸스 간첩소녀 spygirls 콜레스테롤에그",
+    singer: "간첩소녀 (Spygirls)",
+    album: "콜레스테롤 에그 (2017)",
+    title: "콜레스테롤 에그",
+  },
+  {
+    id: 3,
+    name: "스파이걸스 간첩소녀 spygirls happy merry winter song",
+    singer: "간첩소녀 (Spygirls)",
+    album: "Happy Merry Winter Song (2019)",
+    title: "Happy Merry Winter Song",
+  },
+  {
+    id: 4,
+    name: "스파이걸스 간첩소녀 spygirls 그여름징했었지",
+    singer: "간첩소녀 (Spygirls)",
+    album: "그 여름, 징했었지 (2021)",
+    title: "그 여름, 징했었지 (Feat. KomZ)",
+  },
+];
+
+export default songs;
