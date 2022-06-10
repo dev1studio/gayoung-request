36b141661e9086d18d816e813cf70fd02514d020
Chloe <gtalk.with.nani@gmail.com>
2022-06-10 16:48:47 +0900

    Add Apple icon

--- pages/_document.js
+++ pages/_document.js
@@ -26,6 +26,10 @@ class MyDocument extends Document {
             rel="manifest"
             href="/manifest.json"
           />
+          <link
+            rel="apple-touch-icon"
+            href="/icon-192x192.png"
+          />
           <meta
             name="theme-color"
             content="#fff"
