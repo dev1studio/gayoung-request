0f338a948050314351e6e733fa371b2ff3d3669d
Chloe <gtalk.with.nani@gmail.com>
2022-06-10 16:08:49 +0900

    Head setting

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="ko-KR">
         <Head>
           <link
             rel="preconnect"
@@ -17,6 +17,18 @@ class MyDocument extends Document {
           <link
             rel="stylesheet"
             href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard-dynamic-subset.css"
+          />
+          <link
+            rel="icon"
+            href="/favicon.ico"
+          />
+          <link
+            rel="manifest"
+            href="/manifest.json"
+          />
+          <meta
+            name="theme-color"
+            content="#fff"
           />
         </Head>
         <body>
