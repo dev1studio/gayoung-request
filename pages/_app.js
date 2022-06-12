c3f0c288b6f8feb82d5501c9739751234a08ee86
Chloe <gtalk.with.nani@gmail.com>
2022-06-12 16:57:53 +0900

    service-worker working

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from '@emotion/styled';
 import '../styles/globals.sass'
 import Header from '../components/layout/header';
@@ -9,13 +9,30 @@ const Container = styled.div({
 })
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      window.addEventListener("load", function () {
+        navigator.serviceWorker.register("/sw.js").then(
+          function (registration) {
+            console.log(
+              "Service Worker registration successful with scope: ",
+              registration.scope
+            );
+          },
+          function (err) {
+            console.log("Service Worker registration failed: ", err);
+          }
+        );
+      });
+    }
+  }, []);
+
   return (
     <Container>
       <Header />
       <Component {...pageProps} />
       <Footer />
     </Container>
-
   )
 }
 
