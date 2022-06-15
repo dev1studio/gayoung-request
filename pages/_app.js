dea8deeeea27b568f3a47e6c1db6fa7f39c309bc
Chloe <gtalk.with.nani@gmail.com>
2022-06-15 17:23:33 +0900

    Change font loader
    - Pretendard 폰트 불러오는 방식을 개선하다

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import Script from 'next/script'
 import styled from '@emotion/styled';
 import '../styles/globals.sass'
 import Header from '../components/layout/header';
@@ -32,6 +33,22 @@ function MyApp({ Component, pageProps }) {
       <Header />
       <Component {...pageProps} />
       <Footer />
+      <Script
+        id="typekit"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(d) {
+              let config = {
+                kitId: 'rdt1yjr',
+                scriptTimeout: 3000,
+                async: true
+              },
+              h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+            })(document);
+          `,
+        }}
+      />
     </Container>
   )
 }
